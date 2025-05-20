2fd526fee2008efc2f5e8533b9030617ce8340ba
sagarrao1 <sagarrao1@gmail.com>
2025-05-20 20:56:47 +0530

    converted Naveen Core_Java project as Maven project

---
+++ Naveen_Java/Core_Java/src/chapter4/Output.java
@@ -0,0 +1,15 @@
+package chapter4;
+
+public class Output {
+
+	public static void main(String[] args) {
+		int a=3;
+		int b=6;
+		
+		int result = (~a);
+		System.out.println(result);
+				
+
+	}
+
+}
